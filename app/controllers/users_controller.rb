929d5aa0ccda7a73decfe2412ff9fd7ff8d31b16
Di Livius <di_livius@yahoo.co.uk>
2011-09-22 14:59:01 +0300

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,23 @@ class UsersController < ApplicationController
   def new
     @user = User.new
 	@title = "Sign Up"
+	
   end
 
   def show
     @user = User.find(params[:id])
     @title = @user.name
+  end
+  
+  def create
+	@user = User.new(params[:user])
+	if @user.save
+		#Handle a successful save action
+		#/*flash[:success] = "Welcome to my Tweet App"*/
+		redirect_to @user, :flash => { :success => "Welcome to my Tweet App!"}
+	else
+		@title = "Sign Up"
+		render 'new'
+	end
   end
 end
